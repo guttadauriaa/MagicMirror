efeaa200bdd2ecebdfd3ff6f34094196f0bba8c8
Martin Lehouck <martin.lehouck@gmail.com>
2024-04-05 13:26:10 +0200

    erreur 2eme ecran

--- config/config2.js
+++ config/config2.js
@@ -1,25 +1,47 @@
 //ajout 2eme ecran (2)
 var config = {
 	electronOptions: { x: 1080, y: 0},
-	address: "0.0.0.0", // can be whatever you set as your original.
-	port: 8081, // Must be different than the other configuration file.
-	ipWhitelist: [], // Can be whatever you set as your original.
-	language: "en",
-	timeFormat: 12,
-	units: "imperial",
-	//logLevel: ["INFO", "LOG", "WARN", "ERROR", "DEBUG"],
+	address: "localhost",	// Address to listen on, can be:
+							// - "localhost", "127.0.0.1", "::1" to listen on loopback interface
+							// - another specific IPv4/6 to listen on a specific interface
+							// - "0.0.0.0", "::" to listen on any interface
+							// Default, when address config is left out or empty, is "localhost"
+	port: 8080,
+	basePath: "/",	// The URL path where MagicMirror² is hosted. If you are using a Reverse proxy
+									// you must set the sub path here. basePath must end with a /
+	ipWhitelist: ["127.0.0.1", "::ffff:127.0.0.1", "::1"],	// Set [] to allow all IP addresses
+									// or add a specific IPv4 of 192.168.1.5 :
+									// ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.1.5"],
+									// or IPv4 range of 192.168.3.0 --> 192.168.3.15 use CIDR format :
+									// ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.3.0/28"],
+
+	useHttps: false,			// Support HTTPS or not, default "false" will use HTTP
+	httpsPrivateKey: "",	// HTTPS private key path, only require when useHttps is true
+	httpsCertificate: "",	// HTTPS Certificate path, only require when useHttps is true
+
+	language: "fr",
+	locale: "fr-BE",
+	logLevel: ["INFO", "LOG", "WARN", "ERROR"], // Add "DEBUG" for even more logging
+	timeFormat: 24,
+	units: "metric",
+
 	modules: [
-        {
+		
+		
+		
+		{
 			module: "helloworld",
 			position: "middle_center",
 			header: "Petit mot du jour",
 			config: {
 				text: "Bonjour à tous, bienvenue à la faculté !"
 			}
-		}
-	] // end of modules
-}; // end of config variable.
-/*************** DO NOT EDIT BELOW ***************/
-if (typeof module !== "undefined") {
-	module.exports = config;
-}
+		},
+		
+		
+	]
+};
+
+/*************** DO NOT EDIT THE LINE BELOW ***************/
+if (typeof module !== "undefined") { module.exports = config; }
+
