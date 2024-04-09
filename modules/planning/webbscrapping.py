7c03b30d06cafa17986618eae5ee7bbd8612e53e
Martin Lehouck <martin.lehouck@gmail.com>
2024-04-09 11:04:51 +0200

    Mise à jour du chemin du script Python dans webbscrapping.py

--- modules/planning/webbscrapping.py
+++ modules/planning/webbscrapping.py
@@ -14,7 +14,7 @@ horaire = [52, 5] #formation, option
 semaine = 25
 
 #pour interagir avec le site web 
-service = Service(executable_path = "/usr/bin/chromedriver.exe")
+service = Service(executable_path = "/usr/bin/chromedriver")
 driver = webdriver.Chrome(service = service)
 
 driver.get("https://hplanning2023.umons.ac.be/invite")
