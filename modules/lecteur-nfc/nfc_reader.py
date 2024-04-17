79c97fbbfffd9d051030b7616d750e7282e54480
Hugo <hudarv17@outlook.be>
2024-04-17 09:16:25 +0200

    création d'un code simple pour lire nfc

---
+++ modules/lecteur-nfc/nfc_reader.py
@@ -0,0 +1,14 @@
+#!home/MirrorPyEnv/bin python3
+
+import RPi.GPIO as GPIO
+
+from mfrc522 import SimpleMFRC522
+
+reader = SimpleMFRC522
+
+try:
+    id, text = reader.read()
+    print(id, text)
+
+finally:
+    GPIO.cleanup()
