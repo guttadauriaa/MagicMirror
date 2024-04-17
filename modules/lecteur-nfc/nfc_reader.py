3083e37ee8974c03e3370f869fa92fcbe89ddc6c
Hugo <hudarv17@outlook.be>
2024-04-17 10:59:14 +0200

    lecture

--- modules/lecteur-nfc/nfc_reader.py
+++ modules/lecteur-nfc/nfc_reader.py
@@ -6,17 +6,19 @@ from mfrc522 import SimpleMFRC522
 
 reader = SimpleMFRC522()
 
+# try:
+#     text = input ('New data: ')
+#     print("Now place your tag to write")
+#     reader.write (text)
+#     print ("Written" )
+
+# finally:
+
+
 try:
-    text = input ('New data: ')
-    print("Now place your tag to write")
-    reader.write (text)
-    print ("Written" )
+    id, text = reader.read()
+    print(id, text)
+    
 
 finally:
-    try:
-        id, text = reader.read()
-        print(id, text)
-        
-
-    finally:
-        GPIO.cleanup()
+    GPIO.cleanup()
