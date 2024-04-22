e234543c0f98eee74c51d0b38378dd19cc099f2b
Hugo <hudarv17@outlook.be>
2024-04-22 22:21:38 +0200

    amélioration fonctionnailté vocale

--- modules/MMM-voice_control/fonctionnalite.py
+++ modules/MMM-voice_control/fonctionnalite.py
@@ -1,22 +1,25 @@
 class Fonctionnalite:
     def questions(self, data):
 
-        liste_local = ["Auditoire 03", "Auditoire 05" ,"IGlab" ,"Auditoire 12" ,
+        liste_local = ["Auditoire 3", "Auditoire 5" ,"IG lab" ,"Auditoire 12" ,
                        "Auditoire 11" ,"Labo physique",
                        "Auditoire 21" ,"Auditoire 22" ,"Auditoire 23" ,"Auditoire 24" ,"Auditoire 25"]
 
-        liste_test = ["auditoire trois", "auditoire cinq", "ig lab", "auditoire douze", 
-                      "auditoire onze", "labo physique", "auditoire vingt-quatre", 
-                      "auditoire vingt et un", "auditoire vingt-deux", "auditoire vingt-trois", "auditoire vingt-cinq"]
+        # liste_test = ["auditoire trois", "auditoire cinq", "ig lab", "auditoire douze", 
+        #               "auditoire onze", "labo physique", "auditoire vingt-quatre", 
+        #               "auditoire vingt et un", "auditoire vingt-deux", "auditoire vingt-trois", "auditoire vingt-cinq"]
         
-        if any(x in data for x in liste_test):
-            print("ok")
+       
+        for local in liste_local:
+            if local.lower() in data.lower():
+                print("très bien voici comment se rendre à :", local)
+                break
 
-        elif data.find('auditoire') >= 0 or data.find('local') >= 0:
-            print("dans quel local souhaitez vous aller ? ")
-            for i in liste_local:
-                print(i)
-
+        else:
+            if 'auditoire' in data or 'local' in data:
+                print("Dans quel local souhaitez-vous aller ? ")
+                for local in liste_local:
+                    print(local)
 
         
         #guide moi vers un locale == > vers quel local
