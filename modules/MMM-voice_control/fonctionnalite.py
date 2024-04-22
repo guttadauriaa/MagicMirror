0a2abc364a684512d8b2dd6edfc28aa4efee143e
Hugo <hudarv17@outlook.be>
2024-04-22 16:27:51 +0200

    maj detection local

--- modules/MMM-voice_control/fonctionnalite.py
+++ modules/MMM-voice_control/fonctionnalite.py
@@ -2,10 +2,14 @@ class Fonctionnalite:
     def questions(self, data):
 
         liste_local = ["Auditoire 03", "Auditoire 05" ,"IGlab" ,"Auditoire 12" ,
-                       "Auditoire 11" ,"Labo physique A" ,"Labo physique B" ,"Labo physique C ",
+                       "Auditoire 11" ,"Labo physique",
                        "Auditoire 21" ,"Auditoire 22" ,"Auditoire 23" ,"Auditoire 24" ,"Auditoire 25"]
 
-        if any(x in data for x in liste_local):
+        liste_test = ["auditoire trois", "auditoire cinq", "ig lab", "auditoire douze", 
+                      "auditoire onze", "labo physique", "auditoire vingt-quatre", 
+                      "auditoire vingt et un", "auditoire vingt-deux", "auditoire vingt-trois", "auditoire vingt-cinq"]
+        
+        if any(x in data for x in liste_test):
             print("ok")
 
         elif data.find('auditoire') >= 0 or data.find('local') >= 0:
