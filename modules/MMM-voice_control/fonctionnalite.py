59ea400b01dbcfa0989284c5198d23905148accc
Hugo <hudarv17@outlook.be>
2024-04-22 14:44:50 +0200

    maj

--- modules/MMM-voice_control/fonctionnalite.py
+++ modules/MMM-voice_control/fonctionnalite.py
@@ -1,31 +1,12 @@
 class Fonctionnalite:
     def questions(self, data):
-        data = data
-        print(data.find('ok'))
 
-
-        if data.find('framboise') >= 0:
-            print ("oui")
-
-        if (data.find('guide') >= 0 or data.find('amène') >= 0) and data.find('local') >= 0:
-            print("dans quel local souhaitez vous aller ? \n",
-                  "Auditoire 03 \nAuditoire 05 \nIGlab \nAuditoire 12 \nAuditoire 11 \nLabo physique A \nLabo physique B \nLabo physique C ",
-                  "\nAuditoire 21 \nAuditoire 22 \nAuditoire 23 \nAuditoire 24 \nAuditoire 25")
-            
-        if data.find('éteint') >= 0 and data.find('rouge') >= 0:
-            print(") éteint la rouge")
-            
-        if data.find('allume') >= 0 and data.find('verte') >= 0:
-            print("J allume la verte")
-            
-        if data.find('éteint') >= 0 and data.find('verte') >= 0:
-            print("J éteint la verte")
-            
-        if data.find('allume') >= 0 and data.find('bleu') >= 0:
-           print("J allume la bleu")
-           
-        if data.find('éteint') >= 0 and data.find('bleu') >= 0:
-            print("J éteint la bleu")
-            
+        liste_local = ["Auditoire 03", "Auditoire 05" ,"IGlab" ,"Auditoire 12" ,
+                       "Auditoire 11" ,"Labo physique A" ,"Labo physique B" ,"Labo physique C ",
+                       "Auditoire 21" ,"Auditoire 22" ,"Auditoire 23" ,"Auditoire 24" ,"Auditoire 25"]
+        if data.find('auditoire') >= 0 or data.find('local') >= 0:
+            print("dans quel local souhaitez vous aller ? ")
+            for i in liste_local:
+                print(i)
 
         #guide moi vers un locale == > vers quel local
