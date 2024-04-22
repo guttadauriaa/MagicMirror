c3d648d8cfcaf6a25f60f08fa1db9cf9dec5ea6e
Hugo <hudarv17@outlook.be>
2024-04-22 15:09:04 +0200

    debeug3

--- modules/MMM-voice_control/fonctionnalite.py
+++ modules/MMM-voice_control/fonctionnalite.py
@@ -4,10 +4,9 @@ class Fonctionnalite:
         liste_local = ["Auditoire 03", "Auditoire 05" ,"IGlab" ,"Auditoire 12" ,
                        "Auditoire 11" ,"Labo physique A" ,"Labo physique B" ,"Labo physique C ",
                        "Auditoire 21" ,"Auditoire 22" ,"Auditoire 23" ,"Auditoire 24" ,"Auditoire 25"]
-        
 
-        if any(liste_local) in data:
-            print('ok')
+        if any(x in data for x in liste_local):
+            print("ok")
 
         elif data.find('auditoire') >= 0 or data.find('local') >= 0:
             print("dans quel local souhaitez vous aller ? ")
