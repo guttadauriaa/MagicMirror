5af5bd1d5a56299bb38ecec414d62701ff25b407
Hugo <hudarv17@outlook.be>
2024-04-22 15:39:38 +0200

    change test

--- modules/MMM-voice_control/voice-control.py
+++ modules/MMM-voice_control/voice-control.py
@@ -10,9 +10,6 @@ from vosk import Model, KaldiRecognizer
 
 fonctionnalite = Fonctionnalite()
 
-SILENCE_THRESHOLD = 0.01
-DEFAULT_LANG_MODEL = "fr"
-
 q = queue.Queue()
 
 def int_or_str(text):
@@ -34,15 +31,6 @@ def detect_speech(recognizer, data):
         return True
     return False
 
-def parse_arguments():
-    """Parse command-line arguments."""
-    parser = argparse.ArgumentParser(description="Speech recognition from microphone")
-    parser.add_argument("-f", "--filename", type=str, metavar="FILENAME", help="audio file to store recording to")
-    parser.add_argument("-d", "--device", type=int_or_str, help="input device (numeric ID or substring)")
-    parser.add_argument("-r", "--samplerate", type=int, help="sampling rate")
-    parser.add_argument("-m", "--model", type=str, default=DEFAULT_LANG_MODEL, help="language model; e.g. en-us, fr, nl")
-    return parser.parse_args()
-
 parser = argparse.ArgumentParser(add_help=False)
 parser.add_argument("-l", "--list-devices", action="store_true",help="show list of audio devices and exit")
 args, remaining = parser.parse_known_args()
@@ -59,49 +47,59 @@ parser.add_argument("-m", "--model", type=str, help="language model; e.g. en-us,
 args = parser.parse_args(remaining)
 
 try:
-    args = parse_arguments()
-    model = Model(lang=args.model)
+    if args.samplerate is None:
+        device_info = sd.query_devices(args.device, "input")
+        # soundfile expects an int, sounddevice provides a float:
+        args.samplerate = int(device_info["default_samplerate"])
+        
+    if args.model is None:
+        model = Model(lang="fr")
+    else:
+        model = Model(lang=args.model)
+
+    if args.filename:
+        dump_fn = open(args.filename, "wb")
+    else:
+        dump_fn = None
 
     with sd.RawInputStream(samplerate=args.samplerate, blocksize = 8000, device=args.device,dtype="int16", channels=1, callback=callback):
         print("#" * 80)
         print("Press Ctrl+C to stop the recording")
         print("#" * 80)
 
-
-        recognizer = KaldiRecognizer(model, args.samplerate)
+        rec = KaldiRecognizer(model, args.samplerate)
         recording = False
-        
-        #rec = KaldiRecognizer(model, args.samplerate)
-        while True:
-            data = q.get()
 
+        while True:
+            # data = q.get()
             # if rec.AcceptWaveform(data):
             #     parler = json.loads(rec.FinalResult())
-            #     print (parler['text'])
+            #     print (parler ['text'])
             #     fonctionnalite.questions(parler['text'])
 
             # if dump_fn is not None:
             #     dump_fn.write(data)
 
 
+            data = q.get()
+
             if not recording:
-                if detect_speech(recognizer, data):
+                if detect_speech(rec, data):
                     print("Recording started")
                     recording = True
 
             if recording:
-                if detect_speech(recognizer, data):
-                    spoken_text = json.loads(recognizer.FinalResult())['text']
+                if detect_speech(rec, data):
+                    spoken_text = json.loads(rec.FinalResult())['text']
                     fonctionnalite.questions(spoken_text)
 
                 if args.filename:
                     with open(args.filename, "ab") as f:
                         f.write(data)
 
-            if recording and not detect_speech(recognizer, data):
+            if recording and not detect_speech(rec, data):
                 print("Recording stopped")
                 recording = False
-
 
 except KeyboardInterrupt:
     print("\nDone")
