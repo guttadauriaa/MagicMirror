c2ce99d104fe24101e5562529355fb38aecff5fe
Hugo <hudarv17@outlook.be>
2024-04-22 14:36:25 +0200

    test reco local

--- modules/MMM-voice_control/fonctionnalite.py
+++ modules/MMM-voice_control/fonctionnalite.py
@@ -4,11 +4,13 @@ class Fonctionnalite:
         print(data.find('ok'))
 
 
-        if data.find('framboises') >= 0:
+        if data.find('framboise') >= 0:
             print ("oui")
 
-        if data.find('allume') >= 0 and data.find('rouge') >= 0:
-            print("J allume la rouge")
+        if (data.find('guide') >= 0 or data.find('amène') >= 0) and data.find('local') >= 0:
+            print("dans quel local souhaitez vous aller ? \n",
+                  "Auditoire 03 \nAuditoire 05 \nIGlab \nAuditoire 12 \nAuditoire 11 \nLabo physique A \nLabo physique B \nLabo physique C ",
+                  "\nAuditoire 21 \nAuditoire 22 \nAuditoire 23 \nAuditoire 24 \nAuditoire 25")
             
         if data.find('éteint') >= 0 and data.find('rouge') >= 0:
             print(") éteint la rouge")
