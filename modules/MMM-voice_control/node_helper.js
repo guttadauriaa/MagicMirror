0193de60bddba89bdea264a1449f8d295f9327ed
Hugo <hudarv17@outlook.be>
2024-04-24 10:49:43 +0200

    test voice control

--- modules/MMM-voice_control/node_helper.js
+++ modules/MMM-voice_control/node_helper.js
@@ -7,8 +7,19 @@ module.exports = NodeHelper.create({
 
     socketNotificationReceived: function(notification, payload) {
         if (notification === 'VOICE_TEXT') {
-            console.log("Received text from voice recognition:", payload);
-            this.sendSocketNotification('DISPLAY_TEXT', payload);
+
+            exec(`python3 ./modules/MMM-voice_control/voice_control.py `, (error, stdout, stderr) => {
+                if (error) {
+                    console.error(`Erreur d'exécution du script Python: ${error}`);
+                    return;
+                }
+
+                // stdout est la sortie de votre script Python
+                this.sendSocketNotification('DISPLAY_TEXT', stdout);
+            });
+
+            //console.log("Received text from voice recognition:", payload);
+            //this.sendSocketNotification('DISPLAY_TEXT', payload);
         }
     }
 });
