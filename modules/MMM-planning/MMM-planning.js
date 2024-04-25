a5d4d098edf051b887f6928f9038fb88d0364c3d
Martin Lehouck <martin.lehouck@gmail.com>
2024-04-26 01:19:08 +0200

    Correction de la valeur initiale de NFCid dans le module MMM-planning

--- modules/MMM-planning/MMM-planning.js
+++ modules/MMM-planning/MMM-planning.js
@@ -1,7 +1,7 @@
 Module.register("MMM-planning", {
     start: function() {
       this.data = '';
-      this.NFCid = '';
+      this.NFCid = 0;
       // this.sendSocketNotification('START_NFC', {});
     },
     
