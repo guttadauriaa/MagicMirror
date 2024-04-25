504de768b8dbc1f6a73f32b3c1ce59ed4ee549ec
Martin Lehouck <martin.lehouck@gmail.com>
2024-04-26 01:53:02 +0200

    Correction de la gestion des erreurs dans la fonction getDom du module MMM-planning.js

--- modules/MMM-planning/MMM-planning.js
+++ modules/MMM-planning/MMM-planning.js
@@ -1,39 +1,16 @@
-Module.register("MMM-planning", {
-    start: function() {
-      this.data = 'datavide';
-      console.log("Starting module: " + this.name + this.data);
-      this.NFCid = '';
-      this.sendSocketNotification('START_NFC', {});
-    },
-    
-    
-    //s'execute à chaque fois qu'il reçoit une notification
-    socketNotificationReceived: function(notification, payload) {
-      let wrapper = document.getElementById('MMM-planning');
-      Log.info('MMM-planning received a socket notification: ' + notification + ' - Payload: ' + payload);
-      if (notification === 'Planning' && this.NFCid !== '') {
-        this.data = payload;
-        // this.updateDom();
-      }
-      if (notification === 'NFC') {
-        this.NFCid = payload;
-        wrapper.innerHTML = "EN attente des données" + this.NFCid;
-        // this.updateDom();
-        console.log("maj dom",this.NFCid);
-      }
-    },
+Module.register("test_module", {
+  start: function() {
+      this.NFCid = null;
+      this.sendSocketNotification('START_PYTHON_SCRIPT', {});
+      console.log("Starting module: " + this.name);
+  },
 
-    getDom: function() {
-      try {
-          console.log("dom1");
-          let wrapper = document.createElement("div");
-          console.log("dom2");
-          wrapper.id = "MMM-planning";
-  
-          wrapper.innerHTML = "Scannez votre carte d'étudiant -> ";
-          return wrapper;
-      } catch (error) {
-          console.error("Erreur dans getDom: ", error);
-      }
+
+  getDom: function() {
+      console.log("dom1");
+      let wrapper = document.createElement("div");
+      wrapper.id = "test_module";
+      wrapper.innerHTML = "Exécution du script Python, veuillez patienter...";
+      return wrapper;
   }
 });
