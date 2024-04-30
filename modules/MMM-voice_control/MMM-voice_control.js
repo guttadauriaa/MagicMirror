6b0e3d573d99f07c7244611f8078b234115242ac
Hugo <hudarv17@outlook.be>
2024-04-30 09:08:44 +0200

    remise voc tout

--- modules/MMM-voice_control/MMM-voice_control.js
+++ modules/MMM-voice_control/MMM-voice_control.js
@@ -1,6 +1,6 @@
 Module.register("MMM-voice_control", {
     start: function() {
-        this.sendSocketNotification('KEYWORD', {});
+        this.sendSocketNotification('VOICE_TEXT', {});
         console.log("Starting module: " + this.name);
     },
 
@@ -24,12 +24,13 @@ Module.register("MMM-voice_control", {
 
             // Attendre 5 secondes
             setTimeout(() => {
-                this.sendSocketNotification('KEYWORD', {});
+                this.sendSocketNotification('VOICE_TEXT', {});
                 wrapper.innerHTML = `<h1>Attendez...</h1>`;
                 // Attendre 2 secondes supplémentaires
                 setTimeout(() => {
                     if (wrapper) {
-                        wrapper.innerHTML = `<h1> Dites : "miroir" pour demander quelque chose </h1>`;
+                        //wrapper.innerHTML = `<h1> Dites : "miroir" pour demander quelque chose </h1>`;
+                        wrapper.innerHTML = `<h1> Demandez moi quelque chose </h1>`;
                     }
                 }, 2000);
             }, 5000);
@@ -41,7 +42,8 @@ Module.register("MMM-voice_control", {
     getDom: function() {
         let wrapper = document.createElement("div");
         wrapper.id = "MMM-voice_control";
-        wrapper.innerHTML = `<h1> Dites : "miroir" pour demander quelque chose </h1>`;
+        //wrapper.innerHTML = `<h1> Dites : "miroir" pour demander quelque chose </h1>`;
+        wrapper.innerHTML = `<h1> Demandez moi quelque chose </h1>`;
         return wrapper;
     }
 });
