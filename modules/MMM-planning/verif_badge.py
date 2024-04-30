c6f2b1dc91796b57bf314cbf22916c78617ff66b
Hugo <hudarv17@outlook.be>
2024-04-30 10:26:17 +0200

    test

--- modules/MMM-planning/verif_badge.py
+++ modules/MMM-planning/verif_badge.py
@@ -1,11 +1,11 @@
 import sys
 
-NFCid= sys.argv[1]
-test = False
-with open("./modules/MMM-planning/NFCtoH.txt", 'r') as f:
-    for line in f:
-        if NFCid in line:
-            test = True 
-            break
+# NFCid= sys.argv[1]
+# test = False
+# with open("./modules/MMM-planning/NFCtoH.txt", 'r') as f:
+#     for line in f:
+#         if NFCid in line:
+#             test = True 
+#             break
 
-print(test)
+# print(test)
