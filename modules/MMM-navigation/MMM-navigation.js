64752d2958fbf6f5504908f73c48b31807905f94
Yani <yanihuart@gmail.com>
2024-05-08 15:15:06 +0200

    test 12

--- modules/MMM-navigation/MMM-navigation.js
+++ modules/MMM-navigation/MMM-navigation.js
@@ -6,7 +6,7 @@ Module.register("MMM-navigation", {
         wrapper.id = "MMM-navigation";
         let html = "";
         
-        const imagePath = "./modules/MMM-navigation/map/test.png";
+        const imagePath = "./modules/MMM-navigation/map/12.png";
         
         
         html += `<img src="${imagePath}" alt="Your Image" style="width: 70%; height: 70%;" />`;
