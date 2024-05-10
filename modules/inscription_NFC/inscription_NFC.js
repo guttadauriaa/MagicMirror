96c4b395d7794d398185a0e18543c1879ea1722f
Martin Lehouck <martin.lehouck@gmail.com>
2024-05-10 16:31:34 +0200

    Mettre à jour la méthode de réception des notifications dans le fichier inscription_NFC.js

--- modules/inscription_NFC/inscription_NFC.js
+++ modules/inscription_NFC/inscription_NFC.js
@@ -5,21 +5,22 @@ Module.register("inscription_NFC", {
   },
 
   notificationReceived: function(notification, payload) {
+    let wrapper = document.getElementById('inscription_NFC');
+
     if (notification === 'SETUP_BADGE'){
       this.NFCid = payload.badge;
       this.show();
       console.log('inscription_NFC received a notification: ' + notification + ' with payload: ' + payload.badge);
+      html = `<h1> Dites le numéro de votre année d'étude ou "annuler" pour arrêter</h1>`;
+            
+      html += `<p>(1) BAB1<br>(2) BAB2<br>(3) BAB3<br>(4) MA1<br>(5) MA2</p>`;
+      wrapper.innerHTML = html;
+      this.sendSocketNotification('demande_annee', {});
     }
   },
 
   resume: function() {
     this.sendNotification("HIDE_VOICE_CONTROL", {});
-    let wrapper = document.getElementById('inscription_NFC');
-
-    html = `<h1> Dites le numéro de votre année d'étude ou "annuler" pour arrêter</h1>`;
-            
-    html += `<p>(1) BAB1<br>(2) BAB2<br>(3) BAB3<br>(4) MA1<br>(5) MA2</p>`;
-    wrapper.innerHTML = html;
   },
   
   getDom: function() {
