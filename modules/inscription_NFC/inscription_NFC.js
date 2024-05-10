75f6073ae356f6ebd32663fc201b6d6a58296187
Martin Lehouck <martin.lehouck@gmail.com>
2024-05-10 17:35:15 +0200

    Mettre à jour la méthode de réception des notifications dans le fichier inscription_NFC.js

--- modules/inscription_NFC/inscription_NFC.js
+++ modules/inscription_NFC/inscription_NFC.js
@@ -15,19 +15,18 @@ Module.register("inscription_NFC", {
         setTimeout(() => {
           wrapper.innerHTML = "<h1>Je n'ai pas compris, veuillez répéter.</h1>";
         }, 2000);
+      }else{
+        this.NFCid = payload.badge;
+        this.show();
+        console.log('inscription_NFC received a notification: ' + notification + ' with badge: ' + payload.badge);
+        
+        this.sendSocketNotification('demande_annee', {});
+        setTimeout(() => {
+          let html = `<h1> Dites le numéro de votre année d'étude ou "annuler" pour arrêter</h1>`;
+          html += `<p>(1) BAB1<br>(2) BAB2<br>(3) BAB3<br>(4) MA1<br>(5) MA2</p>`;
+          wrapper.innerHTML = html;
+        }, 2000);
       }
-
-      this.NFCid = payload.badge;
-      this.show();
-      console.log('inscription_NFC received a notification: ' + notification + ' with payload: ' + payload.badge);
-      
-      this.sendSocketNotification('demande_annee', {});
-      setTimeout(() => {
-        let html = `<h1> Dites le numéro de votre année d'étude ou "annuler" pour arrêter</h1>`;
-        html += `<p>(1) BAB1<br>(2) BAB2<br>(3) BAB3<br>(4) MA1<br>(5) MA2</p>`;
-        wrapper.innerHTML = html;
-      }, 2000);
-      
     }
     if (notification === 'retour_des_formations'){
       let html = `${payload}`;
