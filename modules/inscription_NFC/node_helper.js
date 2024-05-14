d3a41c97cd076f6357844958b929b1e6d17a4c83
Martin Lehouck <martin.lehouck@gmail.com>
2024-05-14 10:38:19 +0200

    fix: Update node_helper.js to include console log for the output of 'ecouter' socket notification

--- modules/inscription_NFC/node_helper.js
+++ modules/inscription_NFC/node_helper.js
@@ -10,26 +10,14 @@ module.exports = NodeHelper.create({
     socketNotificationReceived: function(notification, payload) {
         console.log("node_hepler inscription_NFC received: " + notification + ' '+ payload.suivant);
         if (notification === "ecouter"){
-            let retryCount = 0;
-
             exec(`/home/miroir/MirrorPyEnv/bin/python3 ./modules/MMM-voice_control/ecouter.py `, (error, stdout, stderr) => {
                 if (error) {
                     console.error(`Erreur d'exécution du script Python ecouter.py`);
-
-                    // Si une erreur se produit et que nous n'avons pas encore réessayé, réessayons une fois
-                    if (retryCount < 1) {
-                        retryCount++;
-                        exec(`/home/miroir/MirrorPyEnv/bin/python3 ./modules/MMM-voice_control/ecouter.py `, (error, stdout, stderr) => {
-                            if (error) {
-                                console.error(`Erreur d'exécution du script Python ecouter.py lors de la deuxième tentative`);
-                            }
-                            this.sendSocketNotification(payload.suivant, {sortie :stdout});
-                        });
-                    }
-                } else {
-                    console.log("[ecouter] La sortie est :", stdout);
-                    this.sendSocketNotification(payload.suivant, {sortie :stdout});
                 }
+
+                console.log("[ecouter] La sortie est :", stdout);
+                this.sendSocketNotification(payload.suivant, {sortie :stdout});
+                
             });
         }
 
