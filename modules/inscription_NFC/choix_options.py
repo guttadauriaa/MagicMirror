492c590d579b579abf6379551d8789173fe0a88a
Martin Lehouck <martin.lehouck@gmail.com>
2024-05-17 01:44:55 +0200

    Update `choix_options.py` to include new options for formation selection

--- modules/inscription_NFC/choix_options.py
+++ modules/inscription_NFC/choix_options.py
@@ -106,8 +106,8 @@ while True:
 options2 = []
 for i in range(len(options)):
     options2.append({'id': i, 'option': options[i]})
-options2 = {'options': options2}
-print(json.dumps(options2))
+options3 = {'options': options2}
+print(json.dumps(options3))
 
 #time.sleep(5)
 driver.quit()
