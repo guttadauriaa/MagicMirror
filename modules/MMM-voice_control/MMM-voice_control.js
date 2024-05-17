052f37273ba3b730e721fc8a7fbe507961586f90
Martin Lehouck <martin.lehouck@gmail.com>
2024-05-17 15:40:37 +0200

    chore: Update MMM-voice_control.js to adjust column gap in HTML output

--- modules/MMM-voice_control/MMM-voice_control.js
+++ modules/MMM-voice_control/MMM-voice_control.js
@@ -36,7 +36,7 @@ Module.register("MMM-voice_control", {
                 if (lines1[1]){
                     html += `<h2>Pour réactiver le contrôle vocal secouez votre main au-dessus de l'heure</h2>`;
                 
-                    html += `<div style="column-count: 2; column-gap: 20px;">`;
+                    html += `<div style="column-count: 2; column-gap: 10px;">`;
 
                     for (let i = 1; i < lines1.length; i++){
                         html += `<p style="font-size: 30px;">${lines1[i]}</p>`;
